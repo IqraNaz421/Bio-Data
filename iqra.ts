a7fdd1ed95df39e1a4663dafc179af38efcde635
Iqra Naz <161621730+IqraNaz421@users.noreply.github.com>
2024-04-03 01:40:38 +0500

    Add files via upload

---
+++ iqra.ts
@@ -0,0 +1,150 @@
+//Bio Data
+let myname: string = "Iqra Naz";
+console.log(`1) My name is ${myname}`);
+
+let fathername: string = "Muhammad asgar";
+console.log(`2) My father name is ${fathername}`);
+
+let age: number = 20;
+console.log(`3) My age is ${age}`);
+
+let marital_status: string = "single";
+console.log(`4) I am ${marital_status}`);
+
+let height: number = 5.6;
+console.log(`5) My height is ${height}`);
+
+let country: string = "Pakistan";
+console.log(`6) My country name is ${country}`);
+
+let city: string = "Karachi";
+console.log(`7) My city name is ${city}`);
+
+let muslim: boolean = true;
+console.log(`8) I am muslim ${muslim}`);
+
+let religion: string = "Islam";
+console.log(`9) My religion is ${religion}`);
+
+let educated: boolean = true;
+console.log(`10) I am educated ${educated}`);
+
+let education: string = "Intermediate of medical science";
+console.log(`11) My education is ${education}`);
+
+let dateofbirth: string = "12-02-2004";
+console.log(`12) I was born in ${dateofbirth}`);
+
+let hobbies: string = "Reading,painting,Eating,Travelling";
+console.log(`13) My hobbies is ${hobbies}`);
+
+let course: string = "Artificial Intelligence";
+console.log(`14) I am doing a ${course} course in Governor House`);
+
+let goal:string = "web developer";
+console.log(`15) I want to become a proficient ${goal}`);
+
+let cast: string = "Sheikh";
+console.log(`16) My cast is ${cast}`);
+
+let favorite_colour: string = "Purple";
+console.log(`17) My favorite colour is ${favorite_colour}`);
+
+let favorite_animals: string = "Lion,Peacock,Giraffe";
+console.log(`18) My favorite animals is ${favorite_animals}`);
+
+let favorite_personality:string = "Quaid-e-Azam Muhammad Ali Jinnah";
+console.log(`19) My favorite personality is ${favorite_personality}`);
+
+let favorite_dish: string = "Biryani";
+console.log(`20) I like to eat ${favorite_dish}`);
+
+let favorite_game: string = "Bubble shooter";
+console.log(`21) My favorite game is ${favorite_game}`);
+
+let favorite_country: string = "Turkey";
+console.log(`22) My favorite country is ${favorite_country}`);
+
+let favorite_place: string = "Sea side";
+console.log(`23) My favorite place is ${favorite_place}`);
+
+let favorite_sports: string = "Racket,Cricket";
+console.log(`24) My favorite sports is ${favorite_sports}`);
+
+let wish: string = "Perform umrah with my parents";
+console.log(`25) I want to ${wish}`);
+
+let favorite_subject: string = "Biology";
+console.log(`26) My favorite subject is ${favorite_subject}`);
+
+let is_intelligent: boolean = true;
+console.log(`27) I am very intelligent ${is_intelligent}`);
+
+let is_beautifull: boolean = true;
+console.log(`28) I am beautifull ${is_beautifull}`);
+
+let matric_result: string = "89%"
+matric_result = "Pass"
+matric_result = "Aone";
+console.log(`29) My matric result is ${matric_result} grade with 89%`);
+
+let intermediate_result: string = "80%"
+intermediate_result = "Pass"
+intermediate_result = "A";
+console.log(`30) My intermediate result is ${intermediate_result} grade with 80%`);
+
+let favorite_nationalsong: string = "Dil Dill Pakistan"
+favorite_nationalsong = "Shukriya Pakistan";
+console.log(`31) My favorite national song is ${favorite_nationalsong}`);
+
+let favorite_poet: string = "Allama Iqbal";
+console.log(`32) My favorite poet is ${favorite_poet}`);
+
+let favorite_cricketer: string = "Shahid Afridi"
+favorite_cricketer = "Babar Azam";
+console.log(`33) My favorite cricketer is ${favorite_cricketer}`);
+
+let favorite_holybook: string = "Quran Majeed";
+console.log(`34) My favorite holy book is ${favorite_holybook}`);
+
+let interest: string = "Coding";
+console.log(`35) I am interested in ${interest}`);
+
+let favorite_seaside: string = "Neelum Valley";
+console.log(`36) My favorite seaside is ${favorite_seaside}`);
+
+let skill: string = "learning typescript";
+console.log(`37) I am currently ${skill} as a skill`);
+
+let language: string = "Urdu"
+language = "urdu speaking";
+console.log(`38) I am ${language}`);
+
+let nationality: string = "Pakistani";
+console.log(`39) I am ${nationality}`);
+
+
+let siblings: string = "1 sister and 1 brother";
+console.log(`40) I have ${siblings}`);
+
+let school_name: string = " Shaheen public english cambridge school";
+console.log(`41) My school name is ${school_name}`);
+
+let clg_name: string = "For women clg for nazimabad";
+console.log(`42) My clg name is ${clg_name}`);
+
+let favorite_teacher: string = "Miss Hira";
+console.log(`43) My favorite teacher is ${favorite_teacher}`);
+
+let gender: string = "Girl";
+console.log(`44) I am a ${gender}`);
+
+const truthspeaker: string = "I speak the truth";
+console.log(`45) ${truthspeaker}`);
+
+const happiness: string = "I am always happy";
+console.log(`46) ${happiness}`);
+
+//I m thankful for completing 150 lines of my bio data
+const closingline: string = "Thank you for considering my bio data";
+console.log(`47) ${closingline}`);
